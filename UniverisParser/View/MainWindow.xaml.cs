6fa992aabd52dbbc5ebeeb09c95f96505257d946
markls <isxakov.mark@gmail.com>
2022-12-28 01:26:54 +0500

    Создан базовый функционал, осуществляется авторизация и парсинг оценки по названию дисциплины и контрольной точки.

---
+++ UniverisParser/View/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using UniverisParser.ViewModel;
+
+namespace UniverisParser.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private MarkViewModel viewModel;
+        private readonly Parser.Parser parser;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = (Resources["ViewModel"] as MarkViewModel)!;
+            parser = new Parser.Parser(viewModel);
+            DataContext = viewModel;
+        }
+
+        private async void ParsingBtnClicked(object sender, RoutedEventArgs e)
+        {
+            parser.Login = LoginTextBox.Text;
+            parser.Password = PasswordTextBox.Text;
+            var discipline = DisciplineTextBox.Text;
+            var controlPoint = ControlPointTextBox.Text;
+            await parser.ParseAsync(discipline, controlPoint);
+        }
+    }
+}
---
+++ UniverisParser/ViewModel/MarkViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UniverisParser.ViewModel;
+
+public class MarkViewModel : INotifyPropertyChanged
+{
+    private string point = "-1";
+    private string rating = "-1";
+
+    public string Point
+    {
+        get => point;
+        set
+        {
+            point = value;
+            OnPropertyChanged(nameof(MarkText));
+        }
+    }
+    public string Rating
+    {
+        get => rating;
+        set
+        {
+            rating = value;   
+            OnPropertyChanged(nameof(MarkText));
+        }
+    }
+    public string MarkText => $"Текущая оценка: {Point}.\nПроцентов: {Rating}";
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
