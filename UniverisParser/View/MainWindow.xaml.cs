2a346f33f7da19f0ad14935edd33074a025d3728
markls <isxakov.mark@gmail.com>
2022-12-28 17:31:12 +0500

    Улучшена работа обработчиков события, добавлена обработка ошибок, создана кнопка прерывания парсинга, создана обработка прерывания задачи. Веб-клиент на каждый запрос парсинга создается новый и после удаляется, чтобы не было проблем с авторизацией.

--- UniverisParser/View/MainWindow.xaml.cs
+++ UniverisParser/View/MainWindow.xaml.cs
@@ -1,30 +1,59 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
 using UniverisParser.ViewModel;
 
-namespace UniverisParser.View
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow
-    {
-        private MarkViewModel viewModel;
-        private readonly Parser.Parser parser;
-        public MainWindow()
-        {
-            InitializeComponent();
-            viewModel = (Resources["ViewModel"] as MarkViewModel)!;
-            parser = new Parser.Parser(viewModel);
-            DataContext = viewModel;
-        }
+namespace UniverisParser.View;
 
-        private async void ParsingBtnClicked(object sender, RoutedEventArgs e)
+public partial class MainWindow
+{
+    private readonly CancellationTokenSource cts = new();
+    private CancellationToken ct;
+    private MarkViewModel viewModel;
+    private readonly Parser.Parser parser;
+    public MainWindow()
+    {
+        InitializeComponent();
+        viewModel = (Resources["ViewModel"] as MarkViewModel)!;
+        ct = cts.Token;
+        parser = new Parser.Parser(viewModel);
+        DataContext = viewModel;
+    }
+
+    private async void ParsingBtnClicked(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        btn!.Click -= ParsingBtnClicked;
+        CancellBtn.IsEnabled = true;
+        ParsingBtn.IsEnabled = false;
+        ExceptionBlock.Text = "";
+        parser.Login = LoginTextBox.Text;
+        parser.Password = PasswordTextBox.Text;
+        var discipline = DisciplineTextBox.Text;
+        var controlPoint = ControlPointTextBox.Text;
+        try
         {
-            parser.Login = LoginTextBox.Text;
-            parser.Password = PasswordTextBox.Text;
-            var discipline = DisciplineTextBox.Text;
-            var controlPoint = ControlPointTextBox.Text;
-            await parser.ParseAsync(discipline, controlPoint);
+            await parser.ParseAsync(discipline, controlPoint, ct);
         }
+        catch (OperationCanceledException)
+        {
+            ExceptionBlock.Text = "Парсинг прерван.";
+        }
+        catch (Exception ex)
+        {
+            ExceptionBlock.Text = ex.Message;
+        }
+        finally
+        {
+            ParsingBtn.IsEnabled = true;
+            CancellBtn.IsEnabled = false;
+            btn.Click += ParsingBtnClicked;
+        }
+    }
+
+    private void CancellationBtnClicked(object sender, RoutedEventArgs e)
+    {
+        cts.Cancel();
     }
 }
